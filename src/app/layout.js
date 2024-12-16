b04efdc44a4c2229017729599dc13d4578745855
Habibur Rahman Jony <habiburrahmanjony4@gmail.com>
2024-12-16 06:59:12 -0500

    fixed the issue with hydration and reverted the previous pages.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,9 +3,8 @@ import "./globals.css";
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <header>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </header>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+
       <body>{children}</body>
     </html>
   );
